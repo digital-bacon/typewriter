52cbec88fac14180b632c6458ebe84e9a70633d6
Mark Bieganek <mark.bieganek@telus.com>
2022-11-22 13:50:15 -0700

    Typewriter will execute a callback after completion

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,17 @@
-const typeWriter = (string, actionForEachCharacter, delay = 25) => {
+const typeWriter = (string, actionForEachCharacter, actionAfterTypeWriter, delay = 25) => {
   let timer = 0;
   for (const charIndex in string) {
     setTimeout(() => actionForEachCharacter(string[charIndex]), (timer += delay));
   }
+  setTimeout(() => actionAfterTypeWriter(), timer);
 }
 
 const doStdOut = (string) => process.stdout.write(string);
 
+const printNewLine = () => console.log('\n');
+
 // TEST CASE:
 const sentence = "hello there from lighthouse labs";
 // Print each letter in the sentence to the terminal with a 50ms delay
-typeWriter(sentence, doStdOut, 50);
+// When done, print a new line
+typeWriter(sentence, doStdOut, printNewLine, 50);
