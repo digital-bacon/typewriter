341a2f7324a2323765db2a8927ab131109c8cf00
Mark Bieganek <mark.bieganek@telus.com>
2022-11-22 13:57:39 -0700

    Add JSDoc comments

--- typewriter.js
+++ typewriter.js
@@ -1,3 +1,14 @@
+/**
+ * Function that prints each character of a given string to the node
+ * terminal
+ * @param {string} string - The string to print to the terminal
+ * @param {Function} actionForEachCharacter - Callback to execute for each
+ * character in the string
+ * @param {Function} actionAfterTypeWriter - Callback to execute after the
+ * string has been printed to the terminal
+ * @param {number} [delay] - The delay between printing each new character to
+ * the terminal
+ */
 const typeWriter = (string, actionForEachCharacter, actionAfterTypeWriter, delay = 25) => {
   let timer = 0;
   for (const charIndex in string) {
@@ -6,8 +17,15 @@ const typeWriter = (string, actionForEachCharacter, actionAfterTypeWriter, delay
   setTimeout(() => actionAfterTypeWriter(), timer);
 };
 
+/**
+ * Prints a given string to the node terminal
+ * @param {string} string - The string to print to the terminal
+ */
 const doStdOut = (string) => process.stdout.write(string);
 
+/**
+ * Prints a new line with `console.log`
+ */
 const printNewLine = () => console.log('\n');
 
 // TEST CASE:
